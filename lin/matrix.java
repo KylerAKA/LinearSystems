c3d3b12884889cdeda8b9852d325f068f93648ee
KylerAKA <kander20@oswego.edu>
2016-06-06 11:33:07 -0400

    matrix filler

--- lin/matrix.java
+++ lin/matrix.java
@@ -1,5 +1,47 @@
 package lin;
 
 public class matrix {
+	int[][] matrix;
+	
+	int n, m;
+	
+	public matrix(int rows, int cols) {
+		n = rows;
+		m = cols;
+		matrix = new int[n][m];
+	}
+	
+	public matrix(int[][] initial) {
+		matrix = initial;
+		n = initial.length;
+		m = initial[0].length;
+	}
+	
+	public boolean isSquare() {
+		return n == m;
+	}
+	
+	public boolean setItem(int r, int c, int a) {
+		try {
+			matrix[r][c] = a;
+		}
+		catch (IndexOutOfBoundsException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public String toString() {
+		String m = "[";
+		for (int[] r: matrix) {
+			m += "[";
+			for (int a: r)
+				m += a + "\t";
+			m += "],\n";
+		}
+		m += "]";
+		
+		return m;
+	}
 	
 }
