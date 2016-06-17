ab4f2782d40f173d060e6216fcf48ad7fdc1cc34
mitchphillipson <mitch.phillipson@gmail.com>
2016-06-17 10:02:53 -0400

    Fixed the solver and updated output to count the q-analog

--- lin/Solver.java
+++ lin/Solver.java
@@ -1,83 +1,102 @@
 package lin;
 
 public class Solver {
-
+	public static int rows_i = 3, cols_i = 3, prime = 2;
 	
-//	Matrix A;
+	public static int num_pivots = 2;
 	
-//	public Solver(Matrix B) {
-//		A = B;
-//	}
+	public static int[] numbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 	
-	public static boolean all(Matrix A,int row,int colm_num){
-		for (int j = 0; j < colm_num; j++)
-			if (A.matrix[row][j] != 0)
-				return false;
-		return true;
-	}
-	
+	public static int rows_f = 10, cols_f = 10;
 	
 	public static int find_pivot(Matrix A, int colm_num) {
-		for (int i = 0; i < A.getNumRow(); i++){
+		Rows: for (int i = 0; i < A.m; i++) {
 			if (A.matrix[i][colm_num] == 0)
 				continue;
-			if(all(A,i,colm_num))
-				return i;
+			for (int j = 0; j < colm_num; j++)
+				if (A.matrix[i][j] != 0)
+					continue Rows;
+			return i;
 		}
 		return -1;
 	}
 	
-	public static int[] find_pivot_colms(Matrix A) {
-		int p = 0;
-		int[] pivot_columns = new int[A.getNumColm()];
-		//int count = 0;
-		for (int j = 0; j < A.getNumColm(); j++) {
+	public static boolean[] find_pivot_colms(Matrix A, int p) {
+		boolean[] pivot_columns = new boolean[A.n];
+		for (int j = 0; j < A.n; j++) {
 			int i = find_pivot(A, j);
 			if (i == -1) {
 				continue;
 			}
 			else {
-				for (int r = i + 1; r < A.getNumRow(); r++) {
-					for (int c = A.getNumColm()-1; c > j - 1; c--) {
-						int a = (A.matrix[i][j] * A.matrix[r][c]) - (A.matrix[r][j]* A.matrix[i][c]);
-						A.setItem(r, c, a);
+				for (int r = i + 1; r < A.m; r++) {
+					for (int c = A.n - 1; c > j - 1; c--) {
+						A.matrix[r][c] = (A.matrix[i][j] * A.matrix[r][c]) - (A.matrix[r][j]
+							* A.matrix[i][c]);
 						if (p != 0)
 							A.matrix[r][c] %= p;
 					}
 				}
 			}
-			pivot_columns[j] = 1;
-			//count++;
+			pivot_columns[j] = true;
 		}
 		return pivot_columns;
 	}
 	
+		
+	
+	
+	
+	
 	public static void main(String... args) {
-		int rows = 2, cols = 2, p = 2;
+		//int rows = 2, cols = 2, p = 2;
 		
-		MatrixPerm mp = new MatrixPerm(rows, cols, p);
+		int p = 2;
+		for(int rows = 2;rows<10;rows++){
+			for(int cols = rows;cols<8;cols++){
+
+				if(Math.pow(2,rows*cols)>Math.pow(2,30))
+					break;
+				
+				MatrixPerm mp = new MatrixPerm(rows, cols, p);
+				
+				int[] pivs = new int[cols+1];
+				for(int i=0;i<cols+1;i++){
+					pivs[i]=0;
+				}
+				
+				Matrix C = mp.toStart();
+				int cnt = 0;
+				int total = 0;
+				do {
+					total++;
+					C = mp.getCurrent();
+					boolean[] colms = find_pivot_colms(C,p);
 		
-		Matrix C = mp.toStart();
-		int cnt = 0;
-		int total = 0;
-		do {
-			total++;
-			C = mp.getCurrent();
-			System.out.println(C);
-			int[] colms = find_pivot_colms(C);
-			String out = "";
-			for(int i : colms){
-				out+= i;
+					int sum = 0;
+					for(int i = 0;i<cols;i++){
+						if(colms[i])
+							sum++;
+					}
+					
+					pivs[sum]++;
+					
+				} while (mp.permute());
+				
+				int out = 0;
+				
+				System.out.println("m: "+rows+", n: "+cols+", p: "+p+"\n");
+				
+				for(int i=0;i<cols+1;i++){
+					System.out.println(i+"\t"+pivs[i]);
+					out += Math.pow(2,i)*pivs[i];
+				}
+				
+				System.out.println("\nTotal:\t"+out);
+				
+				System.out.println("-----------------------");
 			}
-			System.out.println(out);
-			if(colms[cols-1]==0){
-				cnt++;
-				System.out.println("eh");
-			}
-			System.out.println("----------");
-		} while (mp.permute());
-		System.out.println(cnt);
-		System.out.println(total);
+		}
 	}
 }
 
